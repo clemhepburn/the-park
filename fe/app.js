114f1b287e8888148dd7f8bfe3a4ece7495a199b
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-08-01 18:46:10 -0700

    some dom rendering ideas :)

--- fe/app.js
+++ fe/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const API = 'http://localhost:7890/api/v1/messages';
 
 async function send(message) {
@@ -14,6 +15,18 @@ async function getMessages() {
   return await fetch(API).then(res => res.json());
 }
 
+
+// dynamic dom behavior and functions
+//on load, display the existing messages in the messages div
+const messages = document.querySelector('.chat > .messages');
+getMessages().then(res => {
+  for (let i = 0; i < res.length; i++) {
+    const message = document.createElement('p');
+    message.innerHTML = `<strong>${res[i].name}</strong>: ${res[i].message}`;
+    messages.insertBefore(message, messages.firstChild); 
+  }
+});
+
 // post button handler
 document.querySelector('.chat > form > button').addEventListener('click', e => {
   e.preventDefault();
@@ -24,7 +37,27 @@ document.querySelector('.chat > form > button').addEventListener('click', e => {
   const message = document.querySelector(cssQuery + '*:last-child').value;
 
   send({ name, message }).then(res => console.log(res));
+
+  // add the message to the chat. i don't know if this is actually getting it from the db or if it's just getting the form text...
+  const newMessage = document.createElement('p');
+  newMessage.innerHTML = `<strong>${name}</strong>: ${message}`;
+  messages.insertBefore(newMessage, messages.firstChild);
+  // add it to the chat without reloading the page
+  document.location.reload('.messages');
+  
+
+
+
+  // clear the inputs
+  document.querySelector(cssQuery + 'input:first-child' + '*:last-child').value = '';
+
 });
 
 // console log messages
 getMessages().then(res => console.log(res));
+
+
+
+
+
+
