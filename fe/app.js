507cac72848160974d336ad43ff9df55726a1bcd
culi <culitif@tuta.io>
2021-08-01 23:15:03 -0700

    heroku api url

--- fe/app.js
+++ fe/app.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-const API = 'http://localhost:7890/api/v1/messages';
+const API = 'https://birch-park.herokuapp.com/api/v1/messages';
 
 async function send(message) {
   return await fetch(API, {
